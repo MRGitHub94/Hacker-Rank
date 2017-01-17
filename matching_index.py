5a089c5b66dfdb3f8a93f0487a7e4954ee2d865d
Rachel <rachelwshen@gmail.com>
2017-01-17 14:53:59 -0800

    updated solution

--- matching_index.py
+++ matching_index.py
@@ -1,17 +1,35 @@
 # Brute Force O(n) runtime 
-def matching_index(nums):
+# def matching_index(nums):
     # for n in range(len(nums)-1):
     #     if nums[n] - n == 0:
     #         return n
     # return -1
-    for index, num in enumerate(nums):
-        # print (index, num)
-        if index == num: # check that it's the same number
-            return index
-    # this is unindented because the for loop needs to check each tuple first
-    return -1
 
-# def matching_index(arr):
+    # for index, num in enumerate(nums):
+    #     # print (index, num)
+    #     if index == num: # check that it's the same number
+    #         return index
+    # # this is unindented because the for loop needs to check each tuple first
+    # return -1
+
+
+# Solution with binary search
+def matching_index(arr):
+    # set variable begin to 0
+    begin = 0
+    end = len(arr) - 1 # because list indices start at 0
+    while begin <= end: 
+        i = int((begin+end)/2) # binary search start in the middle
+        print "i", i, "arr[i]", arr[i], "arr[i] - i", arr[i] - i
+        if (arr[i] - i < 0):
+            begin = i + 1
+            print "begin", begin
+        elif (arr[i] - i == 0):
+            return i
+        else:
+            end = i - 1
+            print "end", end
+    return -1
 
 print matching_index([-8, 0, 2, 5]) 
 #>>> 2
