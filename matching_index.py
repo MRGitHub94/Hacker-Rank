1236634ef35beae99e08e318e5b407ab0c9eb517
Rachel <rachelwshen@gmail.com>
2017-01-20 16:59:10 -0800

    update

--- matching_index.py
+++ matching_index.py
@@ -14,22 +14,22 @@ def matching_index(nums):
 
 
 # Solution with binary search
-# def matching_index(arr):
-#     # set variable begin to 0
-#     begin = 0
-#     end = len(arr) - 1 # because list indices start at 0
-#     while begin <= end: 
-#         i = int((begin+end)/2) # binary search start in the middle
-#         print "i", i, "arr[i]", arr[i], "arr[i] - i", arr[i] - i
-#         if (arr[i] - i < 0):
-#             begin = i + 1
-#             print "begin", begin
-#         elif (arr[i] - i == 0):
-#             return i
-#         else:
-#             end = i - 1
-#             print "end", end
-#     return -1
+def matching_index(arr):
+    # set variable begin to 0
+    begin = 0
+    end = len(arr) - 1 # because list indices start at 0
+    while begin <= end: 
+        i = int((begin+end)/2) # binary search start in the middle
+        print "i", i, "arr[i]", arr[i], "arr[i] - i", arr[i] - i
+        if (arr[i] - i < 0):
+            begin = i + 1
+            print "begin", begin
+        elif (arr[i] - i == 0):
+            return i
+        else:
+            end = i - 1
+            print "end", end
+    return -1
 
 print matching_index([-8, 0, 2, 5]) 
 #>>> 2
