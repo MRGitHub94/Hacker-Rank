0f3ea7dc96b9ecbe2423fb3ab6cc416fc05844ce
Rachel <rachelwshen@gmail.com>
2017-01-24 13:52:37 -0800

    update

--- numbers_and_ranges.py
+++ numbers_and_ranges.py
@@ -19,3 +19,43 @@ def find_range(input_list,input_number):
         if index == input_number:
             output.append(num)
     return output
+
+# class Range(object):
+#     def __init__(self):
+#         self.lower_bound = -1
+#         self.upper_bound = -1
+    
+#     def __init__(self,lower_bound,upper_bound):
+#         self.lower_bound = lower_bound
+#         self.upper_bound = upper_bound
+
+def find_range(input_list,input_number):
+    first = 0
+    lower_bound = -1
+    upper_bound = -1
+    last = len(input_list) - 1
+    while first < last:
+        mid = (first+last)/2
+        if input_list[mid] < input_number:
+            first = mid + 1
+        else:
+            last = mid
+
+    if input_list[first] != input_number:
+        return Range()
+    else:
+        lower_bound = first
+    first = 0
+    last = len(input_list) - 1
+    while first < last:
+        mid = (first + last)/2
+        if (input_list[mid] < input_number + 1):
+            first = mid + 1
+        else:
+            last = mid
+
+    if(input_list[first] == input_number):
+        upper_bound = first
+    else:
+        upper_bound = first-1
+    return Range(lower_bound,upper_bound)
