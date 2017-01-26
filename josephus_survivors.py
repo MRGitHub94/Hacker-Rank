8010095329664663f5fe1eb276aea8f3c8737356
Rachel <rachelwshen@gmail.com>
2017-01-25 20:16:19 -0800

    added comments

--- josephus_survivors.py
+++ josephus_survivors.py
@@ -1,1 +1,23 @@
 # Josephus Survivors 
+
+# Return the node.data of the last survivor
+# Defined node class 
+def find_survivor(num_people, freq):
+    # make a circular LL
+
+    # want this to run until there's only one node left
+    while node.next != node:
+        # range() captures the number of people we skip
+        for i in range(freq - 1):
+            # this one is going to get cut out
+            node = node.next
+
+        # need to change the references of the LL (in this case it's a doubly-
+        # linked list)
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+        # increment in the while loop
+        node = node.next
+
+    return node.data
