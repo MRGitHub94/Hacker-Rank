9b486904af43123aed0058acb0a53e1383ecf2bb
Rachel <rachelwshen@gmail.com>
2017-01-28 15:48:13 -0800

    update

--- binary_search.py
+++ binary_search.py
@@ -1,11 +1,14 @@
 """Binary Search"""
 
 def binary_search(lst, item):
+    # instantiate variables
     first = 0
-    last = len(lst) - 1
+    last = len(lst) - 1 #capture the index
     found = False
 
+    # run this loop until you exhuast all options or find the item
     while first <= last and not found:
+        # midpoint gets rebinded each time the while loop runs
         midpoint = (first + last) // 2
         if lst[midpoint] == item:
             found = True
