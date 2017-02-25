11b77b9c2e7dedb3beab60e571631350270d4906
Rachel <rachelwshen@gmail.com>
2017-02-25 07:16:20 -0800

    update

--- cashregister.js
+++ cashregister.js
@@ -1,16 +1,20 @@
+// Set up the object cashRegister
 var cashRegister = {
-total:0,
-add: function(itemCost){
+    //property 
+    total:0,
+    //property (method) 
+    add: function(itemCost){
     this.total += itemCost;
 },
-scan: function(item) {
+    //another property
+    scan: function(item) {
         switch (item, quantity) {
         case "eggs": this.add(0.98 * quantity); break;
         case "milk": this.add(1.23 * quantity); break;
         case "magazine": this.add(4.99 * quantity); break;
         case "chocolate": this.add(0.45 * quantity); break;
+        }
     }
-}
 };
 
 // scan each item 4 times
@@ -22,3 +26,43 @@ cashRegister.add("chocolate", 4);
 
 //Show the total bill
 console.log('Your bill is '+ cashRegister.total);
+
+// Next Codecademy module
+var cashRegister = {
+    // two properties instantiated
+    total:0,
+    lastTransactionAmount:0,
+//Dont forget to add your property
+add: function(itemCost) {
+    this.total += itemCost;
+    this.lastTransactionAmount = itemCost;
+},
+//scan property
+scan: function(item,quantity) {
+    switch (item) {
+    case "eggs": this.add(0.98 * quantity); break;
+    case "milk": this.add(1.23 * quantity); break;
+    case "magazine": this.add(4.99 * quantity); break;
+    case "chocolate": this.add(0.45 * quantity); break;
+}
+    return true;
+},
+//Add the voidLastTransaction Method here
+voidLastTransaction: function(){
+    this.total -= this.lastTransactionAmount;
+    }
+
+};
+
+cashRegister.scan('eggs',1);
+cashRegister.scan('milk',1);
+cashRegister.scan('magazine',1);
+cashRegister.scan('chocolate',4);
+
+//Void the last transaction and then add 3 instead
+cashRegister.voidLastTransaction();
+cashRegister.scan('chocolate',3);
+
+
+//Show the total bill
+console.log('Your bill is '+cashRegister.total);
